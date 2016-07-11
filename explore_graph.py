69a95d6737bb144aa4e00d8f09f09cc7d7cf52e1
Evgueni Kountikov <kount002@gmail.com>
2016-07-11 13:05:37 -0400

    differences updates for explore

--- explore_graph.py
+++ explore_graph.py
@@ -37,11 +37,29 @@ def norm_varr(df):
     
     return(df)
 
+def anal_prep(df):
+    #prepares the dataframe for expression analysis, log transform it
+    #changes all values <10 to 10
+    #removes clones with all log(10)
+    lc=list(df.columns)
+    lcs=[x for x in lc if x!='Annotation']
+    dfx=df.fillna(0)
+    arr=np.array(dfx[lcs])
+    arr[arr<1]=1
+    dfx[lcs]=pd.DataFrame(arr, index=dfx.index)
+    dfx.replace(to_replace=1, value=np.nan, inplace=True) # prepares for removal of clones with all basal expression (1)
+    dfx.dropna(subset=lcs, how='all', inplace=True)
+    dfx.replace(to_replace=np.nan, value=1, inplace=True)    
+
+    return(dfx)
+
+
 def hist_show(sample, df):
 #use as a storage for pandas histogram mechanics
     fi=df[sample][df[sample>0.0]].hist(bins=100, range=(0, 1000), alpha=0.4)
     #fi=df[['B18201','E20201']][df['E20201']>0][df['B18201']>0].plot.hist(bins=100, range=(0, 1000), alpha=0.4)
     #pay attention to conditions, do not work as a list, use separately
+    #!!! df.hist(bins=40, log=True)
     fi.set_yscale('log')
     plt.savefig('filename.png')
     
@@ -63,7 +81,7 @@ def scat_matrix(): #need lcs
     plt.clf()
     #does not like NaN values; need to replace and fill before using
     #sns.PairGrid has option dropna. consider
-    dfx=df.fillna(0l)
+    dfx=df.fillna(0)
     arr=np.array(dfx[lcs])
     arr[arr<0]=0
     dfx[lcs]=pd.DataFrame(arr, index=dfx.index)
@@ -78,7 +96,7 @@ def scat_matrix(): #need lcs
 
 if len(sys.argv)<2: #check for agrument presence
     print('Usage: graphing.py pickle_df.pkl base_sample \
-    compare_sample [compare_sample2 ...]')
+    substract_sample [substruct_sample2 ...]')
     sys.exit(2)
 
 try:    #check that pickle exists
@@ -94,4 +112,23 @@ lc=list(df.columns)
 print('The DataFrame contains following columns:', lc)
 
 #dfdf=norm_df(df)
-dfar=norm_varr(df)
+if not isinstance(df, pd.core.frame.DataFrame):
+    print('Pickle file is not Pandas DataFrame')
+    sys.exit(2)
+df=norm_varr(df) #normalize data by total count
+del(df['Undetermined'])
+dfx=anal_prep(df) #min expression is log(10)=1
+
+if sys.argv[2] and sys.argv[3]: ###sss samples are not yet implemented
+    qrs=sys.argv[2] #query sample of interest
+    sss=sys.argv[3:] #list of samples to substruct
+    #call make difference df 
+
+    print('Gather 2x fold increased in', qrs, 'over', sss)
+    dff=pd.DataFrame(dfx['Annotation'])
+    #dff[qrs]=1
+    lcf=[x for x in list(dfx.columns) if x!='Annotation' and x!=qrs]
+    for i in lcf:
+        ij=qrs+'/'+i
+        dff[ij]=dfx[qrs]-dfx[i]
+    print('dff dataframe contains expression differences')
